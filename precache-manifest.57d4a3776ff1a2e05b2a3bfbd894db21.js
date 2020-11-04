f202faf40e8d3a05aefd101bfbb0f9abde8436c9
gaqp <gaqp@cin.ufpe.br>
2020-11-04 16:40:00 -0300

    Updates

---
+++ precache-manifest.57d4a3776ff1a2e05b2a3bfbd894db21.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d3cdbabce41ceef0d24b2b3d16d352",
+    "url": "/WebEdu/index.html"
+  },
+  {
+    "revision": "a78ba163f9366d91c82b",
+    "url": "/WebEdu/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "6b148c10d4108e89d0d7",
+    "url": "/WebEdu/static/css/main.f81b6584.chunk.css"
+  },
+  {
+    "revision": "a78ba163f9366d91c82b",
+    "url": "/WebEdu/static/js/2.8c1bc5b9.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/WebEdu/static/js/2.8c1bc5b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b148c10d4108e89d0d7",
+    "url": "/WebEdu/static/js/main.bb179516.chunk.js"
+  },
+  {
+    "revision": "fcb4e340ee8d5bdcb155",
+    "url": "/WebEdu/static/js/runtime-main.3510dfe3.js"
+  },
+  {
+    "revision": "90d47a308d34ca07d57a19b645075d3b",
+    "url": "/WebEdu/static/media/add_desafios.90d47a30.svg"
+  },
+  {
+    "revision": "d1ee6ad50a40e8f8227e800407246807",
+    "url": "/WebEdu/static/media/add_escola.d1ee6ad5.svg"
+  },
+  {
+    "revision": "b4372b3d051a85c3958c8c7d0e14f260",
+    "url": "/WebEdu/static/media/add_jovemACE.b4372b3d.svg"
+  },
+  {
+    "revision": "93ee3bdc10630f3e183a0e654535f4ac",
+    "url": "/WebEdu/static/media/add_pesquisador.93ee3bdc.svg"
+  },
+  {
+    "revision": "ed467759255813bbaaa552b2e031c43d",
+    "url": "/WebEdu/static/media/add_prof_educacao.ed467759.svg"
+  },
+  {
+    "revision": "9d40890e6a714b4d094725f99969a995",
+    "url": "/WebEdu/static/media/add_prof_saude.9d40890e.svg"
+  },
+  {
+    "revision": "e913a1dc0ec0f7a1a047c180403e8e42",
+    "url": "/WebEdu/static/media/add_professor.e913a1dc.svg"
+  },
+  {
+    "revision": "53fa7969020793c37bfdd646487db76e",
+    "url": "/WebEdu/static/media/adicionar.53fa7969.svg"
+  },
+  {
+    "revision": "3be1de676614a30ee61eedef5ad5e300",
+    "url": "/WebEdu/static/media/adicionarAtivo.3be1de67.svg"
+  },
+  {
+    "revision": "2433ccfe833d132bde3faed0d90cf65e",
+    "url": "/WebEdu/static/media/adminIcon.2433ccfe.svg"
+  },
+  {
+    "revision": "628808ada613326774f5677ab99d3d84",
+    "url": "/WebEdu/static/media/atv_off.628808ad.svg"
+  },
+  {
+    "revision": "473270302c05151811a07a3444502908",
+    "url": "/WebEdu/static/media/atv_on.47327030.svg"
+  },
+  {
+    "revision": "ffe8767a09ebd43d64973fb1b7a8d84c",
+    "url": "/WebEdu/static/media/background_nav.ffe8767a.svg"
+  },
+  {
+    "revision": "00cf1d5e6fa78b3ddb4ecf9ea98661c6",
+    "url": "/WebEdu/static/media/bar_chart.00cf1d5e.svg"
+  },
+  {
+    "revision": "2b329227c56b333950056fae3b641958",
+    "url": "/WebEdu/static/media/bar_chartAtivo.2b329227.svg"
+  },
+  {
+    "revision": "c66392550aaf090f5275ed288951ac2a",
+    "url": "/WebEdu/static/media/bubble_off.c6639255.svg"
+  },
+  {
+    "revision": "195fca02ef9a03e9924c06802b81b3a3",
+    "url": "/WebEdu/static/media/bubble_on.195fca02.svg"
+  },
+  {
+    "revision": "171fe79d20de8f567fd86d826086bd00",
+    "url": "/WebEdu/static/media/edit_desafios.171fe79d.svg"
+  },
+  {
+    "revision": "b7b0595ba11f8a80b6279618fd77246c",
+    "url": "/WebEdu/static/media/edit_escola.b7b0595b.svg"
+  },
+  {
+    "revision": "67e7f250db51a852d4bd49fb8815cb45",
+    "url": "/WebEdu/static/media/edit_jovemACE.67e7f250.svg"
+  },
+  {
+    "revision": "5ff46ebb017baad9b46faca451b87e3e",
+    "url": "/WebEdu/static/media/edit_pesquisador.5ff46ebb.svg"
+  },
+  {
+    "revision": "d42f7d9a0c4a9ffde022df1b20e417a0",
+    "url": "/WebEdu/static/media/edit_prof_educacao.d42f7d9a.svg"
+  },
+  {
+    "revision": "d60cd2d33e91a8f98927b82d8dca8d3f",
+    "url": "/WebEdu/static/media/edit_prof_saude.d60cd2d3.svg"
+  },
+  {
+    "revision": "bc9b8bdb7e35bd7e7238c4c48baaf651",
+    "url": "/WebEdu/static/media/edit_professor.bc9b8bdb.svg"
+  },
+  {
+    "revision": "e7007b36414623727323e94c8e437e69",
+    "url": "/WebEdu/static/media/editar.e7007b36.svg"
+  },
+  {
+    "revision": "d68555befb135b46de065d5e91624ecb",
+    "url": "/WebEdu/static/media/editarAtivo.d68555be.svg"
+  },
+  {
+    "revision": "4bcaa6d71ef98d8f8cd44a0c17d2dbf3",
+    "url": "/WebEdu/static/media/fundo_home.4bcaa6d7.png"
+  },
+  {
+    "revision": "9ecf29f5964219f9efab51fcaf46c107",
+    "url": "/WebEdu/static/media/icon.9ecf29f5.svg"
+  },
+  {
+    "revision": "5b1dd573b3e06e95fdae3566dfdaf398",
+    "url": "/WebEdu/static/media/indicators.5b1dd573.svg"
+  },
+  {
+    "revision": "acecb4d857bb297bbcbb549ef9e7e944",
+    "url": "/WebEdu/static/media/indicatorsAtivo.acecb4d8.svg"
+  },
+  {
+    "revision": "4300279396d9859a34a1d79bbff36ecf",
+    "url": "/WebEdu/static/media/jovemAce.43002793.svg"
+  },
+  {
+    "revision": "fc3f3bd10ff50fd7c83b4174de1661c7",
+    "url": "/WebEdu/static/media/logo.fc3f3bd1.svg"
+  },
+  {
+    "revision": "89a6c2d8d827a89fbc66df9b8cda6aaa",
+    "url": "/WebEdu/static/media/notification.89a6c2d8.svg"
+  },
+  {
+    "revision": "c66392550aaf090f5275ed288951ac2a",
+    "url": "/WebEdu/static/media/notificationAtivo.c6639255.svg"
+  },
+  {
+    "revision": "43eda08c59753d81724426a9d4e57f41",
+    "url": "/WebEdu/static/media/professor.43eda08c.svg"
+  },
+  {
+    "revision": "00cf1d5e6fa78b3ddb4ecf9ea98661c6",
+    "url": "/WebEdu/static/media/stats_off.00cf1d5e.svg"
+  },
+  {
+    "revision": "482e7b35421698c1557843e84bfd3fc9",
+    "url": "/WebEdu/static/media/stats_on.482e7b35.svg"
+  },
+  {
+    "revision": "c1a46c5c25c7464b5ee210246ad5b07a",
+    "url": "/WebEdu/static/media/sucess.c1a46c5c.svg"
+  },
+  {
+    "revision": "60406355e1d43044f7eb6d18d706dde5",
+    "url": "/WebEdu/static/media/user.60406355.svg"
+  },
+  {
+    "revision": "9ae9df2d2936dec78455c614b4b3e060",
+    "url": "/WebEdu/static/media/visto.9ae9df2d.svg"
+  },
+  {
+    "revision": "5b69cb73d42180f64ef9d49cc7bebcf9",
+    "url": "/WebEdu/static/media/vistoAtivo.5b69cb73.svg"
+  }
+]);
